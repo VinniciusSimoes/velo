05a61051fabe99f8cd5800b48423d3f0667aa413
VinniciusSimoes <vinniciussilva7@gmail.com>
2026-05-24 17:22:00 -0300

    Mudando o titulo do cenario

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('webapp deve estar online', async ({ page }) => {
+test('A Webapp deve estar online', async ({ page }) => {
   await page.goto('http://localhost:5174')
 
   // Verifica se o título da página contém "Velô by Papito"
