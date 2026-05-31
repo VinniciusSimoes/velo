95c576391f5fd71632d1742b6f2b3eefd3316bc0
VinniciusSimoes <vinniciussilva7@gmail.com>
2026-05-31 17:48:48 -0300

    Resolvendo desafio sem teste id

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -14,13 +14,23 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act
-  await page.getByTestId('search-order-id').fill('VLO-1FZZZF')
-  await page.getByTestId('search-order-button').click()
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-1FZZZF')
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  //outra forma de preencher o campo
+  //await page.getByLabel('Número do Pedido').fill('VLO-1FZZZF')
+
+
+// Desafio 1: Verificar forma de verificar sem data test id
+// Autor: Vinnicius Simões da Silva
 
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-1FZZZF')
+  await expect(page.getByText('VLO-1FZZZF')).toBeVisible({ timeout: 10_000 })
+  await expect(page.getByRole('region', { name: 'Resultado do pedido' })).toContainText('VLO-1FZZZF')
+  // OU ainda, podemos usar o data-testid para verificar o texto do pedido
+  // await expect(page.getByTestId('order-result-card')).toContainText('VLO-1FZZZF');
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByText('APROVADO')).toBeVisible()
+  await expect(page.getByRole('region', { name: 'Resultado do pedido' })).toContainText('APROVADO')
+  // OU ainda, podemos usar o data-testid para verificar o texto do status
+  // await expect(page.getByTestId('order-result-card')).toContainText('APROVADO'); 
 })
